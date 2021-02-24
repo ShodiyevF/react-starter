d36d2cd187dcbcd2494ce0a846c75b5a56a7d1c2
ShodiyevF <fayzulloh6757@gmail.com>
2021-02-24 14:22:39 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
